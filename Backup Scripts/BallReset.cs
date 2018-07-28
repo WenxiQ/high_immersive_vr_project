2b10d1eaf11a9f75e3882b26a4942ab01de266fa
WenxiQ <i-vrstudio@foxmail.com>
2018-07-29 01:18:17 +0800

    hand control

---
+++ Backup Scripts/BallReset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour {
+	public SteamVR_LoadLevel resetLevel;
+	public SteamVR_LoadLevel nextLevel;
+	//public GoalTrigger goalTrigger;
+	private Transform startTrans; //ball initial position
+	public int goalTargets = 2;
+	private int sumOfGoal = 0;
+	bool isHitGround = false;
+
+	void Start () { 
+		startTrans = GetComponent<Transform>(); // initial position of ball
+	}
+	void Update(){
+		
+		if(sumOfGoal == goalTargets){
+			nextLevel.Trigger();
+		}
+	}
+	void OnTriggerEnter (Collider colliBall){
+		if (colliBall.gameObject.CompareTag("Goal")){
+			sumOfGoal = sumOfGoal + 1;
+			Debug.Log("sum of goal = " + sumOfGoal);
+		}
+		//colliPos = colliBall.GetComponent<Transform>().transform.position;  //get collider obj transform component and the position
+		if (colliBall.gameObject.CompareTag("Ground")){
+			isHitGround = true;
+			Destroy(gameObject); //destory this ball object
+			//Instantiate(gameObject,startTrans.transform.position, Quaternion.identity);//reset this ball object
+			sumOfGoal = 0;
+			resetLevel.Trigger();
+		}
+	}
+}
---
+++ Backup Scripts/HandInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandInteraction : MonoBehaviour {
+	public SteamVR_TrackedObject trackedHandObj;
+	public SteamVR_Controller.Device deviceHand;
+	public float throwForce = 2.0f;
+	// Use this for initialization
+	void Start () {
+		trackedHandObj = GetComponent <SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deviceHand = SteamVR_Controller.Input ((int)trackedHandObj.index);
+	}
+
+	void OnTriggerStay(Collider col) {
+		if (col.gameObject.CompareTag ("Throwable")) {
+			if (deviceHand.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+				ThrowObject (col);
+
+			} 
+			else if (deviceHand.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+				
+				GrabObject (col);
+			}
+		}
+	}
+
+	void ThrowObject (Collider coli) {
+		coli.transform.SetParent (null);
+		Rigidbody rigidBody = coli.GetComponent<Rigidbody> ();//
+		rigidBody.isKinematic = false;
+		rigidBody.velocity = deviceHand.velocity * throwForce;
+		rigidBody.angularVelocity = deviceHand.angularVelocity;
+		Debug.Log ("Released the trigger");
+	}
+
+	void GrabObject (Collider coli) {
+		coli.transform.SetParent (gameObject.transform);
+		coli.GetComponent<Rigidbody> ().isKinematic = true;
+		deviceHand.TriggerHapticPulse (durationMicroSec:2000);
+		Debug.Log ("Touching down the trigger to grab an object");
+	}
+
+}
+
